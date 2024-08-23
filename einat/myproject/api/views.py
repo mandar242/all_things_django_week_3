65d092f6db0b4c929b90ea22f6d532f589ac2d32
Einat Pacifici <epacific@redhat.com>
2024-08-23 10:00:30 +0300

    adding serializer to the project

--- einat/myproject/api/views.py
+++ einat/myproject/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from base.models import Item
+from .serializers import ItemSerializer
 
 @api_view(['GET'])
 def getData(request):
-    person = {'name':'Dennis', 'age':28}
-    return Response(person)
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
